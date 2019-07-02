493e16b253ca0aad924ebdbe2e5f8d916f2040c4
Vahan Sahakyan <vahan.sahakyan@bpcarm.org>
2019-07-02 19:15:18 +0400

    vahan-django = allowed host

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,9 @@ SECRET_KEY = 'u@!!2t_pz__hqemj6++h0_j&c$ckk3y4_981-$7@l=d9i#roci'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'vahan-django.herokuapp.com'
+]
 
 
 # Application definition
