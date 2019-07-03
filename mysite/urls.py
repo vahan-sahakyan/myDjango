de60e1cd9eab7fa56faf03f7bc38b3a5f3a13776
Vahan Sahakyan <vahan.sahakyan@bpcarm.org>
2019-07-03 17:03:30 +0400

    static urls

--- mysite/urls.py
+++ mysite/urls.py
@@ -1,9 +1,13 @@
 from django.conf.urls import include
 from django.urls import path
 from django.contrib import admin
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('mainApp.urls')),
     path('news/', include('news.urls'))
 ]
+
+urlpatterns += staticfiles_urlpatterns()
+
