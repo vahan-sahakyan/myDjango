f3bda245fbe38ebd7be0a9fdcdcedeaa9c6109b9
vahan-sahakyan <52359994+vahan-sahakyan@users.noreply.github.com>
2019-07-03 15:49:56 +0400

    myDjango -> app

--- mysite/settings.py
+++ mysite/settings.py
@@ -126,6 +126,6 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
     os.path.join(BASE_DIR, 'mainApp/static'),
-    "/myDjango/mainApp/static/",
+    "/app/mainApp/static/",
     "/mainApp/static/"
 ]
